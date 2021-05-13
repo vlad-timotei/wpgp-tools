c942ce88fc9b50680b84ec68d30ea9aa5d2418b6
Vlad Timotei <65488419+vlad-timotei@users.noreply.github.com>
2021-05-13 11:54:56 +0300

    Rename variable for TM env compatibility
    This is a know/accepted redundancy for TM script that runs in the same environment, compared to the different extension env.

--- src/js/wp-gp-tools-consistency.js
+++ src/js/wp-gp-tools-consistency.js
@@ -1,10 +1,12 @@
-var user_settings = { 'search' : 'enabled', 'prevent_unsaved': 'disabled', 'autocomplete_suggestion' : false}; 
-/** 'prevent_unsaved': 'disabled' because it doesn't work properly yet ToDO: in future version improve algorithm */
+var user_env_settings = { 'search' : 'enabled', 'prevent_unsaved': 'disabled', 'autocomplete_suggestion' : false}; 
+/** 'prevent_unsaved': 'disabled' => it doesn't work properly yet ToDO: in future version improve algorithm 
+	user_env_settings is a known/accepted redundancy for TM script that runs in the same environment, compared to the different extension env
+*/
 
 if(getLS('wpgpt-user-settings') !== null)
-	user_settings = JSON.parse( getLS('wpgpt-user-settings') ); 
+	user_env_settings = JSON.parse( getLS('wpgpt-user-settings') ); 
 
-if( user_settings ['search'] == "enabled"){
+if( user_env_settings['search'] == "enabled"){
 ( function( $ ){
 
 	var tabs = [];
@@ -154,7 +156,7 @@ function elements_init(){
 	$(".wpgpt-google-translate.wpgpt-google-translate").click( function(){ open_tab('google-translate', $(this).data('gt-string') ); } );
 	$(window).on("beforeunload", function(){
 		close_tabs('all');
-		if( user_edited && user_settings['prevent_unsaved'] == "enabled" && $(".editor:visible textarea").val() !== undefined )
+		if( user_edited && user_env_settings['prevent_unsaved'] == "enabled" && $(".editor:visible textarea").val() !== undefined )
 			return false;
 		} );
 	}
@@ -188,7 +190,7 @@ function get_consistency_suggestions(string_id, get_consistency_btn){
 	$(get_consistency_btn).before(list_of_suggestions);
 	$(get_consistency_btn).remove();   
 
-	if( user_settings['autocomplete_suggestion'] && translations.length == 1 )
+	if( user_env_settings['autocomplete_suggestion'] && translations.length == 1 )
 		$(".editor:visible .suggestions__translation-consistency .copy-suggestion:eq(0)").click(); 
 		
 	ifr.remove();
