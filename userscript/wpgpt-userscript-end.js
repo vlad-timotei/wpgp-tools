c7dc428433d7bd7e83fcd60e8a6af3a66b1fa47d
Vlad Timotei <65488419+vlad-timotei@users.noreply.github.com>
2021-05-13 20:02:17 +0300

    m not M

--- userscript/wpgpt-userscript-end.js
+++ userscript/wpgpt-userscript-end.js
@@ -1,4 +1,4 @@
 /*Final UserScript overrides*/
 update_notif_template = '<span class="wpgpt-update-notice"><b>WPGlotPress Tools new version available</b><br>Update to version <b>%%last_version%%</b> (from  ' +
 							WPGPT_VERSION + ')<br><a href="https://github.com/vlad-timotei/wpgp-tools/raw/main/userscript/wpgpt-userscript-latest.user.js"><br> Click here</a>' +
-							' and TamperMonkey will prompt to reinstall the userscript. If that somehow fails, please manually copy the url and install it.';
+							' and Tampermonkey will prompt to reinstall the userscript. If that somehow fails, please manually copy the url and install it.';
