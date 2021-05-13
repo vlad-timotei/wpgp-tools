c97843e71aec909ba72b126adda91f513814af9c
Vlad Timotei <65488419+vlad-timotei@users.noreply.github.com>
2021-05-13 19:53:04 +0300

    Prompt to install userscript
    update URI of the TM script

--- userscript/wpgpt-userscript-end.js
+++ userscript/wpgpt-userscript-end.js
@@ -1,4 +1,4 @@
 /*Final UserScript overrides*/
 update_notif_template = '<span class="wpgpt-update-notice"><b>WPGlotPress Tools new version available</b><br>Update to version <b>%%last_version%%</b> (from  ' +
-							WPGPT_VERSION + ')<br><a href="https://raw.githubusercontent.com/vlad-timotei/wpgp-tools/main/userscript/wpgpt-userscript-main-latest.js"><br> Click here</a>' +
+							WPGPT_VERSION + ')<br><a href="https://github.com/vlad-timotei/wpgp-tools/raw/main/userscript/wpgpt-userscript-latest.user.js"><br> Click here</a>' +
 							', TamperMonkey or GreaseMonkey should take over from there. If not, manually copy the url and install it.';
