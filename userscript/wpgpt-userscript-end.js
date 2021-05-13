60a95eb0d300a6052bd8600a1cf3c433e3a638cd
Vlad Timotei <65488419+vlad-timotei@users.noreply.github.com>
2021-05-13 19:56:26 +0300

    Update the update TM instructions

--- userscript/wpgpt-userscript-end.js
+++ userscript/wpgpt-userscript-end.js
@@ -1,4 +1,4 @@
 /*Final UserScript overrides*/
 update_notif_template = '<span class="wpgpt-update-notice"><b>WPGlotPress Tools new version available</b><br>Update to version <b>%%last_version%%</b> (from  ' +
 							WPGPT_VERSION + ')<br><a href="https://github.com/vlad-timotei/wpgp-tools/raw/main/userscript/wpgpt-userscript-latest.user.js"><br> Click here</a>' +
-							', TamperMonkey or GreaseMonkey should take over from there. If not, manually copy the url and install it.';
+							' and TamperMonkey will prompt to reinstall the userscript. If that somehow fails, please manually copy the url and install it.';
