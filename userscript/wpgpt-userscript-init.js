8feebb189a85ce06088414b1f2acdc8005f0de53
Vlad Timotei <65488419+vlad-timotei@users.noreply.github.com>
2021-06-17 17:54:17 +0300

    Update wpgpt-userscript-init.js

--- userscript/wpgpt-userscript-init.js
+++ userscript/wpgpt-userscript-init.js
@@ -7,4 +7,7 @@ var notice_icon = 'https://github.com/vlad-timotei/wpgp-tools/raw/main/src/img/n
 var cssTxt = GM_getResourceText('wpgpt-style');
 GM_addStyle( cssTxt );
 
-/*Other scripts loaded from the trunk: Settings, Checks, Consistency & Shortcuts */
+/*
+** Other scripts loaded from the trunk: Settings, Checks, Consistency & Shortcuts
+** No need for LS functions, as they are included in wp-gp-tools-consistency.js
+*/
