780d49cdf04eb4b6f2f0fd89f245354ad8f8268b
Vlad Timotei <65488419+vlad-timotei@users.noreply.github.com>
2021-06-17 17:38:57 +0300

    Update wpgpt-userscript-latest.user.js

--- userscript/wpgpt-userscript-latest.user.js
+++ userscript/wpgpt-userscript-latest.user.js
@@ -1,17 +1,17 @@
 // ==UserScript==
-// @name         WPGP Tools
+// @name         WPGPTools
 // @namespace    https://vladtimotei.ro
 // @description  Tools for a faster translation flow
-// @version      1.3
+// @version      1.2
 // @author       Vlad T
 // @grant        GM_addStyle
 // @grant        GM_getResourceText
 // @match        https://translate.wordpress.org/*
 // @resource     wpgpt-style https://raw.githubusercontent.com/vlad-timotei/wpgp-tools/main/src/css/wp-gp-tools-style.css?v=1.2
 // @require      https://raw.githubusercontent.com/vlad-timotei/wpgp-tools/main/userscript/wpgpt-userscript-init.js?v=1.2
-// @require      https://raw.githubusercontent.com/vlad-timotei/wpgp-tools/main/src/js/wp-gp-tools-settings.js?v=1.2compatible
+// @require      https://raw.githubusercontent.com/vlad-timotei/wpgp-tools/main/src/js/wp-gp-tools-settings.js?v=1.2
 // @require      https://raw.githubusercontent.com/vlad-timotei/wpgp-tools/main/src/js/wp-gp-tools-checks.js?v=1.2
-// @require      https://raw.githubusercontent.com/vlad-timotei/wpgp-tools/main/src/js/wp-gp-tools-consistency.js?v=1.2compatibile
+// @require      https://raw.githubusercontent.com/vlad-timotei/wpgp-tools/main/src/js/wp-gp-tools-consistency.js?v=1.2
 // @require      https://raw.githubusercontent.com/vlad-timotei/wpgp-tools/main/src/js/wp-gp-tools-shortcuts.js?v=1.2
 // @require      https://raw.githubusercontent.com/vlad-timotei/wpgp-tools/main/userscript/wpgpt-userscript-end.js?v=1.2
 // ==/UserScript==
