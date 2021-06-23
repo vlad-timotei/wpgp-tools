561d20e3f3b5a4b8bd007228747283b659618966
Vlad Timotei <65488419+vlad-timotei@users.noreply.github.com>
2021-06-23 09:57:40 +0300

    Update wpgpt-userscript-init.js

--- userscript/wpgpt-userscript-init.js
+++ userscript/wpgpt-userscript-init.js
@@ -7,7 +7,7 @@ var notice_icon = 'https://github.com/vlad-timotei/wpgp-tools/raw/main/src/img/n
 var cssTxt = GM_getResourceText('wpgpt-style');
 GM_addStyle( cssTxt );
 
-var update_notif_template = '<div class="wpgpt-update-notice"><strong>WPGPTools v.%s has new features!</strong> You\'re using v.' + WPGPT_VERSION + '. Update now! ' +
+var wpgpt_update_template = '<div class="wpgpt-update-notice"><strong>WPGPTools v.%s has new features!</strong> You\'re using v.' + WPGPT_VERSION + '. Update now! ' +
 							'<br><a href="https://github.com/vlad-timotei/wpgp-tools/raw/main/userscript/wpgpt-userscript-latest.user.js"><br>Click here</a>' +
 							' and Tampermonkey will prompt to reinstall the userscript. If that somehow fails, please manually copy the url and install it.';
 
