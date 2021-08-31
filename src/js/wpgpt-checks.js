decbdd538209a1078aa082d37c18cdd1ab15d898
vlad-timotei <65488419+vlad-timotei@users.noreply.github.com>
2021-08-31 23:06:01 +0300

    Enhancement | Coding standards

--- src/js/wpgpt-checks.js
+++ src/js/wpgpt-checks.js
@@ -743,12 +743,12 @@ function arr_diff( a, b ) {
 function wpgpt_occurrences( string, subString ) {
 	string = `${string.toLowerCase()}`;
 	subString = `${subString.toLowerCase()}`;
-	if ( subString.length <= 0 ) {return ( string.length + 1 );}
+	if ( subString.length <= 0 ) { return ( string.length + 1 ); }
 	let n = 0, pos = 0;
 	const step = subString.length;
 	while ( true ) {
 		pos = string.indexOf( subString, pos );
-		if ( pos >= 0 ) { ++n; pos += step; } else {break;}
+		if ( pos >= 0 ) { ++n; pos += step; } else { break; }
 	}
 	return n;
 }
