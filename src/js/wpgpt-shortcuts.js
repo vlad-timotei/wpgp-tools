34f39581ea122b51eca9219322d0d46380b7befc
vlad-timotei <65488419+vlad-timotei@users.noreply.github.com>
2021-09-20 12:00:04 +0300

    Coding standards | Shortcuts
    Code refactoring | Shortcuts 1

--- src/js/wpgpt-shortcuts.js
+++ src/js/wpgpt-shortcuts.js
@@ -1,63 +1,38 @@
-/*
-* Override GP (again) and add some shortcuts
-* Based on: https://meta.trac.wordpress.org/browser/sites/trunk/wordpress.org/public_html/wp-content/plugins/wporg-gp-customizations/templates/js/editor.js#L143
-*/
+// Based on: https://meta.trac.wordpress.org/browser/sites/trunk/wordpress.org/public_html/wp-content/plugins/wporg-gp-customizations/templates/js/editor.js#L143
 if ( typeof $gp_editor_options !== 'undefined' ) {
 	$gp.editor.keydown = ( function( original ) {
 		return function( event ) {
-			if ( 13 === event.keyCode && event.shiftKey ) {			// Shift-Enter = Save.
-				const $textarea = jQuery( event.target );
-				if ( ! $textarea.val().trim() ) {
-					$gp.notices.error( 'Translation is empty.' );
-					return false;
-				}
-				const $textareas = $gp.editor.current.find( '.textareas:not(.active) textarea' );
-				let isValid = true;
-				$textareas.each( function() { if ( ! this.value.trim() ) { isValid = false;	} } );
-				if ( ! isValid ) {
-					$gp.notices.error( 'Translation is empty.' );
-					return false;
-				}
-				const translation_e_id = `#${$gp.editor.current.attr( 'id' )}`;
-				const translation_p_id = translation_e_id.replace( 'editor', 'preview' );
-				/*								wpgpt_next_is_strict = true		wpgpt_next_is_strict = false
-				 * warnings_passd = true		false && true = FALSE			false && false = FALSE
-				 *  warnings_passd = false		true && true = TRUE				true && false = FALSE
-				 */
-				if ( wpgpt_checks_shortcuts && ( ! wpgpt_check_this_translation( translation_e_id, translation_p_id ) ) && wpgpt_next_is_strict ) {
-					$gp.notices.error( wpgpt_error_message );
-					return false;
-				} else {
-					wpgpt_next_is_strict = true;
-					jQuery( '.wpgpt-ignore-warnings input' ).prop( 'checked', false );
-					wpgpt_user_edited = false;
-					$gp.editor.save( $gp.editor.current.find( 'button.translation-actions__save' ) );
-				}
+			if ( 13 === event.keyCode && event.shiftKey ) {	// Shift-Enter = Save.
+				wpgpt_save( event.target.closest( '.editor-panel' ).querySelector( 'button.translation-actions__save' ), event.ctrlKey );
 			} else if ( ( 107 === event.keyCode && event.ctrlKey ) || ( 65 === event.keyCode && event.shiftKey && event.ctrlKey ) ) { // Ctrl-+ or Ctrl-Shift-A = Approve.
-				const approve = event.target.closest( '.editor-panel' ).querySelector( '.approve' );
-				if ( approve ) {
-					const translation_e_id = `#${$gp.editor.current.attr( 'id' )}`;
-					const translation_p_id = translation_e_id.replace( 'editor', 'preview' );
-					if ( wpgpt_checks_shortcuts && ( ! wpgpt_check_this_translation( translation_e_id, translation_p_id ) ) && wpgpt_next_is_strict ) {
-						$gp.notices.error( wpgpt_error_message );
-						return false;
-					} else {
-						approve.trigger( 'click' );
-						jQuery( '.wpgpt-ignore-warnings input' ).prop( 'checked', false );
-						wpgpt_user_edited = false;
-						wpgpt_next_is_strict = true;
-					}
-				}
+				wpgpt_save( event.target.closest( '.editor-panel' ).querySelector( '.approve' ) );
 			} else if ( 'enabled' === wpgpt_settings.shortcuts.state && ( ( 106 === event.keyCode && event.ctrlKey ) || ( 70 === event.keyCode && event.shiftKey && event.ctrlKey ) ) ) { // Ctrl-* (NumPad) or Ctrl-Shift-F = Fuzzy.
-				const reject = jQuery( '.editor:visible' ).find( '.fuzzy' );
-				if ( reject.length > 0 ) {
-					reject.trigger( 'click' );
-				}
+				const fuzzy = event.target.closest( '.editor-panel' ).querySelector( '.fuzzy' );
+				fuzzy && fuzzy.click();
 			} else {
 				return original.apply( $gp.editor, arguments );
 			}
-
 			return false;
 		};
 	} )( $gp.editor.keydown );
+}
+
+/**
+*                         wpgpt_next_is_strict = true  wpgpt_next_is_strict = false
+* warnings_passd = true   false && true = FALSE        false && false = FALSE
+* warnings_passd = false  true && true = TRUE          true && false = FALSE
+*/
+function wpgpt_save( save, forced_save = false ) {
+	if ( forced_save ) {
+		wpgpt_next_is_strict = false;
+	}
+	if ( wpgpt_checks_shortcuts && ! wpgpt_check_this_translation( `#${$gp.editor.current.attr( 'id' )}`, `#${$gp.editor.current.attr( 'id' ).replace( 'editor', 'preview' )}` ) && wpgpt_next_is_strict ) {
+		$gp.notices.error( wpgpt_error_message );
+		return false;
+	} else {
+		document.querySelectorAll( '.wpgpt-ignore-warnings input' ).forEach( ( check ) => { check.checked = false; } );
+		wpgpt_user_edited = false;
+		save.click();
+	}
+	wpgpt_next_is_strict = true;
 }
