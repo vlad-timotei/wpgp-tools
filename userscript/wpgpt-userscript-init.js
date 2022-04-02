0592fbba3959d147e139ac961a5a48a440cfa33c
Vlad Timotei <65488419+vlad-timotei@users.noreply.github.com>
2022-04-02 23:34:48 +0300

    Update wpgpt-userscript-init.js

--- userscript/wpgpt-userscript-init.js
+++ userscript/wpgpt-userscript-init.js
@@ -1,6 +1,8 @@
-/* UserScript Initialization * 
-let wpgpt_warning_icon = 'https://github.com/vlad-timotei/wpgp-tools/raw/main/src/img/warning.png';
-let wpgpt_notice_icon = 'https://github.com/vlad-timotei/wpgp-tools/raw/main/src/img/notice.png';
+/* UserScript Initialization */ 
+const wpgpt_us_assets = {
+    wpgpt_warning_icon: 'https://github.com/vlad-timotei/wpgp-tools/raw/main/src/img/warning.png',
+    wpgpt_notice_icon: 'https://github.com/vlad-timotei/wpgp-tools/raw/main/src/img/notice.png',
+};
 
 const cssTxt = GM_getResourceText( 'wpgpt-style' );
 GM_addStyle( cssTxt );
